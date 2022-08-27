485f0087dc13dfed280e1ec76c41367be0fd9e30
Sampat Sharma <31382436+sampatsharma143@users.noreply.github.com>
2022-08-27 19:03:50 +0530

    Update AppWriteHelper.kt

--- androidApp/src/main/java/com/shunyank/zerocard/android/network/AppWriteHelper.kt
+++ androidApp/src/main/java/com/shunyank/zerocard/android/network/AppWriteHelper.kt
@@ -8,8 +8,8 @@ class AppWriteHelper {
             @Volatile private var INSTANCE: Client? = null
             fun getInstance(context: Context): Client =
                 INSTANCE ?: Client(context)
-                    .setEndpoint("https://superapp.megamind.app/v1")
-                    .setProject("62ad9d00262b2158da33")
+                    .setEndpoint("set appwrite url")
+                    .setProject("set project id")
         }
 
 }
